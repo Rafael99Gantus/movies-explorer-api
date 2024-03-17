d9b7978bb264d0a651817473a60075d85d1b0d7d
Рафаэль Гантус <rafael.gantus@bk.ru>
2024-03-17 23:19:54 +0300

    fix2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 const express = require("express");
 const mongoose = require("mongoose");
-const { errors } = require("celebrate");
+// const { errors } = require("celebrate");
 const helmet = require("helmet");
 const cors = require("cors");
 
