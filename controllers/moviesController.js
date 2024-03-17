36e8f1e1a3d52dece65970c15c916c2f6fe004d3
Рафаэль Гантус <rafael.gantus@bk.ru>
2024-03-18 00:10:08 +0300

    fix

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -44,10 +44,38 @@ module.exports.deleteMovies = async (req, res, next) => {
 // };
 
 module.exports.postMovies = async (req, res, next) => {
-  try {
-    const movie = await Movies.create({ owner: req.user._id, ...req.body }).orFail(() => new NotFoundError(`${ERROR_404}`));
-    res.status(http2.constants.HTTP_STATUS_OK).json(movie);
-  } catch (err) {
-    next(err);
-  }
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+  } = req.body;
+  const owner = req.user._id;
+  Movies
+    .create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      nameRU,
+      nameEN,
+      thumbnail,
+      movieId,
+      owner,
+    })
+    .then((selectedMovie) => selectedMovie.populate("owner"))
+    .then((movie) => res.status(http2.constants.HTTP_STATUS_CREATED).send(movie))
+    .catch((err) => {
+      next(err);
+    });
 };
