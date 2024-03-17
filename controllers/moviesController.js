7104dd09656ea7fa27ff77de2cb421111cd9ad3f
Рафаэль Гантус <rafael.gantus@bk.ru>
2024-03-17 22:22:38 +0300

    fix

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -27,7 +27,7 @@ module.exports.getMoviesId = async (req, res, next) => {
 module.exports.deleteMovies = async (req, res, next) => {
   try {
     const { movieId } = req.params;
-    await Movies.deleteOne({ _id: movieId }).orFail(() => new NotFoundError(`${ERROR_404}`));
+    await Movies.deleteOne(req.params.movieId).orFail(() => new NotFoundError(`${ERROR_404}`));
     res.status(http2.constants.HTTP_STATUS_OK).json(movieId);
   } catch (err) {
     next(err);
